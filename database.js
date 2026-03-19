1338993fcdb6214790fdf45aae10eeff09fcad94
Mayank Maroti <marotimayank@gmail.com>
2026-03-19 14:17:08 -0400

    Remove build script for Railway

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const Database = require("better-sqlite3");
 const path = require("path");
 const dummyData = require("./dummyData");
 
-const DB_PATH = path.join(__dirname, "properties.db");
+const DB_PATH = process.env.DB_PATH || path.join(__dirname, "properties.db");
 const db = new Database(DB_PATH);
 
 // Enable WAL mode for better concurrent read performance
